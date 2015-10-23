d26b62f23fb34e8acb8ddabd7c05952a778a4b2d
jjamie-08 <jjamie_08@hotmail.com>
2015-10-23 23:34:25 +0700

    change join interface

--- src/MainActivity.java
+++ src/MainActivity.java
@@ -1,3 +1,4 @@
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
@@ -7,98 +8,58 @@ import java.awt.event.KeyListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-
-import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JLayeredPane;
 import javax.swing.JOptionPane;
-import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 
+
 public class MainActivity extends JFrame implements KeyListener {
-	private JLayeredPane layeredPane;
 	private String key;
 	private JTextField ssidField;
 	private JPasswordField keyField;
 	private JTextField usernameField;
-	
+
 	public static void main(String[] arg) {
 		new MainActivity();
 	}
 
 	public MainActivity() {
-		this.setSize(400, 350);
+		this.setSize(400, 400);
 		Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
 		this.setLocation(dim.width / 2 - this.getSize().width / 2, dim.height
-				/ 2 - this.getSize().height / 2);
+				/ 2 - this.getSize().height / 2-50);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		layeredPane = new JLayeredPane();
 		addKeyListener(this);
-
-		WindowListener exitListener = new WindowAdapter() {
-			@Override
-			public void windowClosing(WindowEvent e) {
-				int confirm = JOptionPane.showOptionDialog(null,
-						"Are You Sure to Close Application?",
-						"Exit Confirmation", JOptionPane.YES_NO_OPTION,
-						JOptionPane.QUESTION_MESSAGE, null, null, null);
-				if (confirm == 0) {
-					String[] command1 = {"/bin/bash","-c",
-							"echo " + key + "| sudo -S service network-manager start" };
-					try {
-						Process process = Runtime.getRuntime().exec(command1);
-						int returnCode = process.waitFor();
-						System.out.println(command1);
-						System.out.println("Service start:= " + returnCode);
-					} catch (IOException e1) {
-						e1.printStackTrace();
-					}catch (InterruptedException e1) {
-						e1.printStackTrace();
-					}
-					System.exit(0);
-				}
-			}
-		};
-		this.addWindowListener(exitListener);
+		exitListener();
 		frameJoinChannel();
-		this.add(layeredPane);
+		this.setResizable(false);
 		this.setVisible(true);
-
 	}
 
 	public void frameJoinChannel() {
-		layeredPane.removeAll();
-		JPanel join = new JPanel();
-		join.setSize(400, 350);
+		ImagePanel join = new ImagePanel("picture/login.png");
+		join.setSize(400, 400);
+		join.setLayout(null);
 
-		JLabel username = new JLabel();
-		username.setText("Name");
-		username.setBounds(70, 70, 100, 30);
+		ssidField = new JTextField();
+		ssidField.setBounds(120, 170, 200, 30);
+		setStandartTextField(ssidField,"SSID");
+		
+		
 		usernameField = new JTextField();
-		usernameField.setBounds(120, 70, 200, 30);
-
-		JLabel ssid = new JLabel();
-		ssid.setText("SSID");
-		ssid.setBounds(70, 130, 100, 30);
-		ssidField = new JTextField("jamie");
-		ssidField.setBounds(120, 130, 200, 30);
-
-		JLabel key = new JLabel();
-		key.setText("Key");
-		key.setBounds(70, 190, 100, 30);
-		keyField = new JPasswordField("jamie16130");
-		keyField.setBounds(120, 190, 200, 30);
-
-		JButton joinButton = new JButton("Join");
+		usernameField.setBounds(120, 215, 200, 30);
+		setStandartTextField(usernameField,"Username");
+		
+		
+		keyField = new JPasswordField();
+		keyField.setBounds(120, 260, 200, 30);
+		setStandartTextField(keyField,"Password");
+		
+		PictureButton joinButton = new PictureButton("picture/joinButton.png","picture/joinButtonClick.png");
 		joinButton.setText("Join");
-		joinButton.setBounds(150, 250, 100, 30);
+		joinButton.setBounds(64, 316, 261, 35);
 		joinButton.addActionListener(new ActionListener() {
 
 			@Override
@@ -107,9 +68,13 @@ public class MainActivity extends JFrame implements KeyListener {
 					JOptionPane.showMessageDialog(null,
 							"Please fill in your SSID", "Error",
 							JOptionPane.ERROR_MESSAGE);
-				} else if (keyField.getText().isEmpty()) {
+				} else if (usernameField.getText().isEmpty()) {
 					JOptionPane.showMessageDialog(null,
-							"Please fill in your key.", "Error",
+							"Please fill in your username.", "Error",
+							JOptionPane.ERROR_MESSAGE);
+				}else if(keyField.getText().isEmpty()){
+					JOptionPane.showMessageDialog(null,
+							"Please fill in your password.", "Error",
 							JOptionPane.ERROR_MESSAGE);
 				} else {
 					configAdhoc();
@@ -117,17 +82,24 @@ public class MainActivity extends JFrame implements KeyListener {
 			}
 		});
 
-		layeredPane.add(username);
-		layeredPane.add(usernameField);
-		layeredPane.add(ssidField);
-		layeredPane.add(ssid);
-		layeredPane.add(key);
-		layeredPane.add(keyField);
-		layeredPane.add(joinButton);
+		join.add(usernameField);
+		join.add(ssidField);
+		join.add(keyField);
+		join.add(joinButton);
+		this.add(join);
+
 	}
 
+	public void setStandartTextField(JTextField textField,String text){
+		textField.setBackground(new Color(58,65,73));
+		textField.setForeground(Color.white);
+		textField.setBorder(null);
+		textField.setCaretColor(Color.white);
+		TextPrompt tp = new TextPrompt(text, textField);
+		tp.setForeground(new Color(106,116,127));
+	}
+	
 	public void configAdhoc() {
-
 		Process process;
 		try {
 			key = keyField.getText();
@@ -185,11 +157,11 @@ public class MainActivity extends JFrame implements KeyListener {
 
 			int ip = (int) Math.floor((Math.random() * 255) + 1);
 			String[] command8 = { "/bin/bash", "-c",
-					"echo " + key + "| sudo -S ifconfig wlan0 192.168.1."+ip };
+					"echo " + key + "| sudo -S ifconfig wlan0 192.168.1." + ip };
 			process = Runtime.getRuntime().exec(command8);
 			returnCode = process.waitFor();
 			System.out.println(command8);
-			System.out.println("IP 192.168."+ip+":= " + returnCode);
+			System.out.println("IP 192.168." + ip + ":= " + returnCode);
 
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -197,6 +169,22 @@ public class MainActivity extends JFrame implements KeyListener {
 			e.printStackTrace();
 		}
 
+	}
+
+	public void exitListener() {
+		WindowListener exitListener = new WindowAdapter() {
+			@Override
+			public void windowClosing(WindowEvent e) {
+				int confirm = JOptionPane.showOptionDialog(null,
+						"Are You Sure to Close Application?",
+						"Exit Confirmation", JOptionPane.YES_NO_OPTION,
+						JOptionPane.QUESTION_MESSAGE, null, null, null);
+				if (confirm == 0) {
+					System.exit(0);
+				}
+			}
+		};
+		this.addWindowListener(exitListener);
 	}
 
 	@Override
