ce29edd277869ed12cf89a522a3f3195178a4981
git <shimanosk130@hotmail.com>
2015-10-26 22:34:08 +0900

    fixf()

--- src/MainActivity.java
+++ src/MainActivity.java
@@ -9,10 +9,14 @@ import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
 import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.net.InetSocketAddress;
 import java.net.UnknownHostException;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
 
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
@@ -42,33 +46,25 @@ public class MainActivity extends JFrame implements KeyListener {
 		this.setResizable(false);
 		this.setVisible(true);
 	}
-	public void broadcast() throws UnknownHostException, InterruptedException{
 
-	    final String INET_ADDR = "192.168.1.254";
-	    final int PORT = 8000;
+	public void broadcast(AudioChunk audio) throws InterruptedException, IOException{
+		ServerSocketChannel ssChannel = ServerSocketChannel.open();
+        ssChannel.configureBlocking(true);
+        int port = 8000;
+        ssChannel.socket().bind(new InetSocketAddress(port));
 
-	    
-	        // Get the address that we are going to connect to.
-	        InetAddress addr = InetAddress.getByName(INET_ADDR);
+        //String obj ="testtext";
+        while (true) {
+            SocketChannel sChannel = ssChannel.accept();
 
-	        // Open a new DatagramSocket, which will be used to send the data.
-	        try (DatagramSocket serverSocket = new DatagramSocket()) {
-	            for (int i = 0; i < 100; i++) {
-	                String msg = "Sent message no " + i;
+            ObjectOutputStream  oos = new 
+                      ObjectOutputStream(sChannel.socket().getOutputStream());
+            oos.writeObject(audio);
+            oos.close();
 
-	                // Create a packet that will contain the datas
-	                // (in the form of bytes) and send it.
-	                DatagramPacket msgPacket = new DatagramPacket(msg.getBytes(),
-	                        msg.getBytes().length, addr, PORT);
-	                serverSocket.send(msgPacket);
-
-	                System.out.println("Server sent packet with msg: " + msg);
-	                Thread.sleep(500);
-	            }
-	        } catch (IOException ex) {
-	            ex.printStackTrace();
-	        }
+            System.out.println("Connection ended");
 	    }
+        }
 	public void frameJoinChannel() {
 		ImagePanel join = new ImagePanel("picture/login.png");
 		join.setSize(400, 400);
